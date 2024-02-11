caf5f0700f21eccad9aed81762920d536ac816f2
bigmurtaza <muratcansarkalkan@gmail.com>
2024-02-10 20:02:39 -0500

    Initial commit

---
+++ EnableWindowed.cpp
@@ -0,0 +1,69 @@
+#include "plugin-std.h"
+
+using namespace plugin;
+
+namespace nbalive {
+
+    static void METHOD OnSetWindowed05(void* t, DUMMY_ARG, char* a2, char a3, int a4, int a5) {
+        CallMethod<0x7415A0>(t, a2, a3, a4, 1);
+    }
+
+    static void METHOD OnSetWindowed06(void* t, DUMMY_ARG, char* a2, char a3, int a4, int a5) {
+        CallMethod<0x755D70>(t, a2, a3, a4, 1);
+    }
+
+    static void METHOD OnSetWindowed07(int a5) {
+        CallMethod<0x9D90E0>(1);
+    }
+
+    static void METHOD OnSetWindowed08(int a5) {
+        CallMethod<0xA12900>(1);
+    }
+    LPCWSTR section = L"MAIN";
+    LPCWSTR key = L"WINDOWED";
+    LPCWSTR filePath = L".\\wndmode.ini";
+    int defaultValue = 0;  // Provide a default value in case the key is not found
+
+    // Call GetPrivateProfileInt to retrieve the integer value
+    int attributeValue = GetPrivateProfileIntW(section, key, defaultValue, filePath);
+}
+
+void EnableWindowed() {
+    using namespace nbalive;
+    if (attributeValue == 1) {
+    switch (FM::GetEntryPoint()) {
+
+        case 0xCD8005: // NBA Live 2005 1.0 NOCD
+        {
+            patch::RedirectCall(0x7B0B00, OnSetWindowed05);
+            patch::SetChar(0xC4A8D0, 1);
+            break;
+        }
+        case 0x40109F:
+        {
+            if (patch::GetFloat(0xBD832C) == 1.3333334f) { // NBA Live 06 1.0 NOCD
+                patch::RedirectCall(0x7D0BB0, OnSetWindowed06);
+                patch::SetChar(0xCB3748, 1);
+                break;
+            }
+            else if (patch::GetFloat(0xBBBC3C) == 1.3333334f) { // NBA Live 07 1.1 NOCD
+                patch::RedirectJump(0x9D9419, OnSetWindowed07);
+                patch::SetChar(0xCEBBAC, 1);
+                break;
+            }
+            else if (patch::GetFloat(0xC3DF84) == 1.3333334f) { // NBA Live 08 1.0 NOCD
+                patch::RedirectJump(0xA12D8A, OnSetWindowed08);
+                patch::SetChar(0xDA7A1C, 1);
+                break;
+            }
+        }
+    }
+    }
+}
+
+class NBALive {
+public:
+    NBALive() {
+        EnableWindowed();
+    }
+} screenResolution;
